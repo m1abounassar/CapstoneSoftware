7828ae092888e12db1d4e882d420925455e6ca36
eshasingh05 <esingh37@gatech.edu>
2025-03-27 20:53:20 -0400

    fetching from db

--- section-selection/app/student/page.js
+++ section-selection/app/student/page.js
@@ -22,6 +22,8 @@ function Section({ section, index, moveUp, moveDown }) {
 
 export default function Home() {
   const [sections, setSections] = useState([]);
+  const [teams, setTeams] = useState([]);
+  const [curr, setCurr] = useState([]);
 
   useEffect(() => {
       async function fetchSession() {
@@ -39,12 +41,28 @@ export default function Home() {
       fetchSession();
     }, []);
 
+
   useEffect(() => {
     fetch("https://jdregistration.sci.gatech.edu/sections.php")
       .then(response => response.json())
       .then(data => setSections(data.sections))
       .catch(error => console.error('Error fetching sections:', error));
   }, []);
+
+  useEffect(() => {
+    fetch("https://jdregistration.sci.gatech.edu/actualTeams.php")
+      .then(response => response.json())
+      .then(data => setTeams(data.teams))
+      .catch(error => console.error('Error fetching sections:', error));
+  }, []);
+
+  useEffect(() => {
+    fetch("https://jdregistration.sci.gatech.edu/api/auth/session.php")
+      .then(response => response.json())
+      .then(data => setCurr(data.username))
+      .catch(error => console.error('Error fetching sections:', error));
+  }, []);
+
 
   const moveUp = (index) => {
     if (index === 0) return;
@@ -62,165 +80,159 @@ export default function Home() {
 
 
   return (
-    <div className='h-svh overflow-hidden bg-[#E5E2D3] font-figtree hover:cursor-default'>
-      <div className='bg-[#A5925A] grid grid-cols-3 w-681 items-center'>
-        <div className='p-4 text-lg lg:text-2xl w-max text-[#003056]'>
-          Junior Design <span className='pt-0 pb-4 pl-0 text-2xl font-bold text-[#232323]'> Team Sync</span>
-        </div>
-        <div></div>
-        <div className='pt-5 pb-5 pr-4 text-sm lg:text-lg justify-self-end text-[#003056] flex gap-5 items-center'>
+    <div className='h-svh overflow-hidden bg-[#E5E2D3] font-figtree hover:cursor-default flex flex-col'>
+
+        <div className='bg-[#A5925A] grid grid-cols-3 w-681 items-center'>
+              <div className='p-4 text-lg lg:text-2xl w-max text-[#003056]'>
+                Junior Design <span className='pt-0 pb-4 pl-0 text-2xl font-bold text-[#232323]'> Team Sync</span>
+              </div>
+              <div></div>
+              <div className='pt-5 pb-5 pr-4 text-sm lg:text-lg justify-self-end text-[#003056] flex gap-5 items-center'>
+                
+                <div>{curr}</div>
+                <div className='hover:text-[#2b6b9e] hover:cursor-pointer text-xl lg:text-2xl p-0 pb-1'>☰</div>
+
+              </div>
           
-          <div>Student</div>
-          <div className='hover:text-[#2b6b9e] hover:cursor-pointer text-xl lg:text-2xl p-0 pb-1'>☰</div>
-
         </div>
 
 
 
-        
-      </div>
+        {/* Below Header */}
+        <div className='grid grid-rows-14 gap-8 h-[100%] m-10 mb-15'>
 
-      {/* Main Content */}
-      <div className='grid grid-cols-4 gap-10 m-10'>
-        {/* Student Status bar */}
-        <div className='col-span-3'>
-          <div className='px-8 py-2 lg:py-4 bg-white rounded-2xl p-0 text-black text-sm sm:text-s font-bold'> STUDENT MODE   |   You have not yet submitted your section preferences. The deadline to do so is 12/05/2025. </div>
-        </div>
-        {/* Leave Team button */}
-        <div className='flex flex-col space-y-2'>
-          {/* THIS IS A PLACEHOLDER BECAUSE I DON'T KNOW HOW TO NAVIGATE TO THE NEXT PAGE */}
-          {/* ALSO WE DON'T EVEN HAVE A NEXT PAGE TO NAVIGATE TO YET */}
-          <button onClick={() => moveUp(index)} className='font-bold text-lg bg-[#A5925A] px-3 py-2 rounded hover:bg-[#002040] hover:text-white'>Leave Team</button>
-        </div>
-      </div>
+            {/* Panels */}
+            <div className='row-span-12 grid grid-cols-5 gap-10'>
 
-      <div className='grid grid-cols-5 gap-10 m-10'>
-        {/* Sections Panel */}
-        <div className='col-span-2'>
-          <div className='bg-[#003056] w-full h-min rounded-3xl'>
-            <div className='px-8 py-2 lg:py-4 text-white text-lg lg:text-3xl font-bold'>Sections</div>
-            <div className='bg-[#FFFFFF] h-full w-full rounded-b-3xl px-6 py-4 border-5 border-[#003056]'>
-            {sections.length > 0 ? (
-                  sections.map((section) => (
-                    <div key={section.id} className='p-3 bg-[#E5E2D3] rounded-md my-2 shadow-sm text-lg'>
-                      <div className='flex gap-2 items-center text-[#003056]'>  {/* row 1 */}
-                        <div className='font-bold w-auto'>{section.title}</div>
-                        <div className='mr-10'>- {section.time}</div>
 
-                      </div>
-                      <div className='flex'>
-                        
-                        <div className='text-black opacity-40'>{section.capacity} seats remaining</div>
-                      </div>
+
+                {/* Sections */}
+                <div className='col-span-2 bg-[#003056] h-[100%] rounded-3xl flex flex-col'>
+
+                    <div className='px-8 py-2 lg:py-4 text-white text-lg lg:text-3xl font-bold'>Sections</div>
+
+                    <div className='bg-[#FFFFFF] h-full w-full rounded-b-3xl px-6 py-4 border-5 border-[#003056] overflow-auto'>
+                        {sections.length > 0 ? (
+                          sections.map((section) => (
+                            <div key={section.id} className='p-3 pl-6 bg-[#E5E2D3] rounded-md my-2 shadow-sm text-lg'>
+                              <div className='flex gap-2 items-center text-[#003056]'>  {/* row 1 */}
+                                <div className='font-bold w-auto'>{section.title}</div>
+                                <div className='mr-10'>- {section.time}</div>
+
+                              </div>
+                              <div className='flex'>
+                                
+                                <div className='text-black opacity-40'>{section.capacity} seats remaining</div>
+                              </div>
+                            </div>
+                          ))
+                        ) : (
+                          <p>Loading sections...</p>
+                        )}
                     </div>
-                  ))
-                ) : (
-                  <p>Loading sections...</p>
-                )}
+
+
+                </div>
+
+
+                {/* Team */}
+                <div className='col-span-3 bg-[#003056] rounded-3xl h-[100%] flex flex-col'>
+
+                    <div className='px-8 py-2 lg:py-4 text-white text-lg lg:text-3xl font-bold'>Team Info</div>
+
+                    <div className='bg-[#FFFFFF] h-full w-full rounded-b-3xl px-6 py-4 border-5 border-[#003056] overflow-auto'>
+                        {teams.length > 0 ? (
+                              teams.map((team) => (
+                                <div key={team.name}>
+
+                                    <div className='flex pb-3 border-b-2 border-[#A5925A] text-xl text-[#003056]'>
+
+                                      <div className='font-bold text-nowrap'>Team {team.name} - </div>
+
+                                      <div className=' ml-1 text-nowrap'>{team.projectName}</div>
+
+                                      <div className='flex w-full justify-end'>
+                                        <div className='font-bold'>Client:</div>
+                                        <div className='ml-1'>{team.clientName}</div>
+                                      </div>
+
+
+                                    </div>
+
+
+
+                                    
+                                  
+                                </div>
+                              ))
+                            ) : (
+                              <p>Loading team...</p>
+                            )
+                        }
+                    </div>
+
+
+                </div>
+
             </div>
-          </div>
+
+
+
+      
+
+            {/* Buttons */}
+            <div className='row-span-1 grid grid-cols-10'>
+
+
+                <button className='col-span-2 font-bold text-2xl bg-[#A5925A] px-3 py-2 mt-0 rounded-3xl hover:bg-[#002040] hover:text-white'>Save Preferences</button>
+
+                <button className='col-span-2 col-start-9 font-bold text-2xl bg-[#A5925A] px-3 py-2 mt-0 rounded-3xl hover:bg-[#002040] hover:text-white'>Leave Team</button>
+
+
+            </div>
+
+
+
+          
         </div>
-
-
-
-        {/* Team Info Panel */}
-        <div className='col-span-3'>
-          <div className='bg-[#f0f0f0] w-full rounded-3xl h-5/6 overflow-auto'>
-            <div className='px-8 py-2 lg:py-4 text-[#003056] text-lg lg:text-3xl font-bold'>Team Info - 4317</div>
-            <div className='bg-[#f0f0f0] h-full w-full rounded-3xl px-6 py-4 border-5 border-[#003056]'>
-              <p className='text-left text-gray-700 font-bold'>Team Section Preferences</p>
-            </div>
-            <div className='grid grid-cols-4 gap-10'>
-              <div className='cols-span-1'>
-                  <p className='text-right text-gray-700'>You</p>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-[#FF7556] rounded-full align-center'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full align-center'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full align-center'></div>
-              </div>
-              <div className='cols-span-1'>
-                  <p className='text-right text-gray-700'>Esha Singh</p>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-[#FF7556] rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                  <p className='text-right text-gray-700'>Jace Walden</p>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                  <p className='text-right text-gray-700'>Shane Mays</p>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-[#FF7556] rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-[#FFC943] rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                  <p className='text-right text-gray-700'>Cindy Kwok</p>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                  <p className='text-right text-gray-700'>Matthew Abernathy</p>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-[#FFC943] rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                <div className='w-6 h-6 bg-green-300 rounded-full'></div>
-              </div>
-              <div className='cols-span-1'>
-                  <p className='text-right text-gray-700 m-4'>Sections:</p>
-              </div>
-              <div className='cols-span-1'>
-                  <p className='text-left text-gray-700 m-4'>Section A</p>
-              </div>
-              <div className='cols-span-1'>
-                  <p className='text-left text-gray-700 m-4'>Section B</p>
-              </div>
-              <div className='cols-span-1'>
-                  <p className='text-left text-gray-700 m-4'>Section C</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 
 
     </div>
   );
 }
+
+
+
+// {/* Sections Panel */}
+// <div className='col-span-2'>
+//   <div className='bg-[#003056] w-full h-[100%] rounded-3xl'>
+//     <div className='px-8 py-2 lg:py-4 text-white text-lg lg:text-3xl font-bold'>Sections</div>
+
+
+//     <div className='bg-[#FFFFFF] h-full w-full rounded-b-3xl px-6 py-4 border-5 border-[#003056] overflow-auto'>
+//       
+//     </div>
+//   </div>
+// </div>
+
+
+
+// {/* Team Info Panel */}
+// <div className='col-span-3'>
+//   <div className=''>
+//     <div className='px-8 py-2 lg:py-4 text-white text-lg lg:text-3xl font-bold'>Team Info</div>
+//     <div className='bg-white h-full w-full rounded-b-3xl px-6 py-4 border-5 border-[#003056] overflow-auto'>
+//       <p className='text-left text-gray-700 font-bold'>Team Section Preferences</p>
+      
+//     </div>
+//   </div>
+// </div>
+
+// </div>
+
+
+
+
+
+
+
