e06750efe2886005a23115365c18d93e527f672a
Singh, Esha <esingh37@gatech.edu>
2025-03-28 19:11:52 -0400

    Update page.js

--- section-selection/app/student/page.js
+++ section-selection/app/student/page.js
@@ -70,7 +70,7 @@ export default function Home() {
     };
   
     fetchData();
-  }, []);
+  }, [username]);
 
   const handlePriorityChange = (index, newValue) => {
     setPriorities((prev) => ({
