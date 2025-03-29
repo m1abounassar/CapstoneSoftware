88e634245d482ec0b357e9ef7fea46b84e4bd917
Singh, Esha <esingh37@gatech.edu>
2025-03-29 17:41:56 -0400

    Update page.js

--- section-selection/app/student/page.js
+++ section-selection/app/student/page.js
@@ -13,7 +13,7 @@ export default function Home() {
   const [dropdownValues, setDropdownValues] = useState({});
   const [savePrefOpen, setSavePrefOpen] = useState(false);
   const [selectedChoice, setSelectedChoice] = useState("3");
-  const [teamNumber, setTeamNumber] = useState();
+  const [teamNumber, setTeamNumber] = useState(0);
   const [teamMembers, setTeamMembers] = useState([]);
   // New state for the name edit popup
   const [nameEditOpen, setNameEditOpen] = useState(false);
