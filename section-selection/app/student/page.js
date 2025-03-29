c99569b361ddb69481d9c676e6aba6179ebfce48
Singh, Esha <esingh37@gatech.edu>
2025-03-29 19:45:27 -0400

    Update page.js

--- section-selection/app/student/page.js
+++ section-selection/app/student/page.js
@@ -397,8 +397,8 @@ export default function Home() {
 
                                     <div className='pt-3 text-xl text-[#003056] font-bold text-nowrap'>Team Section Preferences</div>
 
-                                    {teamMembers.length > 0 ? (
-                                      teamMembers.map(([name, choices]) => (
+                                    {Object.entries(teamMembers).length > 0 ? (
+                                      Object.entries(teamMembers).length(([name, choices]) => (
                                         <div key={name} className='p-3 pl-6 bg-[#E5E2D3] rounded-md my-2 shadow-sm text-lg grid grid-cols-2 items-center'>
                                           <div>
                                               <div className='flex gap-2 items-center text-[#003056]'>  {/* row 1 */}
