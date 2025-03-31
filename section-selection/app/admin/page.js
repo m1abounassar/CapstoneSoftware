bbf66a700709860616c1d399a5ecde090facefbc
Singh, Esha <esingh37@gatech.edu>
2025-03-31 03:09:26 -0400

    typo

--- section-selection/app/admin/page.js
+++ section-selection/app/admin/page.js
@@ -135,7 +135,7 @@ export default function Home() {
     setNewSection(prev => ({ ...prev, [name]: value }));
   };
 
-    const handleInputChange = (e) => {
+    const handleEditInputChange = (e) => {
       const { name, value } = e.target;
       setSelectedSection(prev => ({ ...prev, [name]: value }));
   };
